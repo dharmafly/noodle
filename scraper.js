5fd669f5af0b82e1ae51e38cc0ef6689faa12ff1
Aaron Acerboni <a.acerboni@gmail.com>
2012-06-29 17:09:03 +0100

    Add scraper

---
+++ scraper.js
@@ -0,0 +1,45 @@
+var jsdom = require('jsdom');
+
+exports.scrape = scrape;
+
+function scrape (query, callback) {
+  if(valid(query)){
+    jsdom.env({
+      html: query.url,
+      scripts: ['http://code.jquery.com/jquery-1.5.min.js'],
+      done: function (error, window) {
+        if (!error) {
+          select(window.jQuery, query.selector, query.extract, callback);
+        } else {
+          callback('error');
+        }
+      }
+    });
+
+  } else {
+    callback('error');
+  };
+}
+
+function select (jq, selector, extract, callback) {
+  var results = [];
+
+  if (extract === 'html') {
+    jq(selector).each(function (i, elem) {
+      results.push(jq(elem).html());
+    });
+    callback(false, JSON.stringify(results));
+  } else if (extract) {
+    jq(selector).each(function (i, elem) {
+      results.push(jq(elem).attr(extract));
+    });
+    callback(false, JSON.stringify(results));
+  } else {
+    callback('error');
+  }
+}
+
+// Check if query supplied is OK
+function valid (query) {
+  return (query.url && query.selector) ? true : false;
+}
