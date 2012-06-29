04a3a8abec59ff49f1168d6d1fe2642e4acbd10f
Aaron Acerboni <a.acerboni@gmail.com>
2012-06-29 14:44:14 +0100

    Add server

---
+++ app.js
@@ -0,0 +1,30 @@
+var connect = require('connect'),
+    http    = require('http'),
+    scraper = require('./scraper');
+
+var app = connect()
+          .use(connect.json())
+          .use(handle);
+
+function handle (req, res) {
+  var query = (Object.keys(req.body).length > 0) ?  req.body : false;
+
+  if (query) {
+    scraper.scrape(query, function (err, results) {
+      finish(res, {error: err, results: results});
+    });
+  } else {
+    finish(res, {error: 'No query'});
+  }
+}
+
+function finish (res, params) {
+  if (params.error) {
+    res.end('nsql: ' + params.error);
+  } else {
+    res.writeHead('200', {'Content-type':'application/json'});
+    res.end(params.results);
+  }
+}
+
+http.createServer(app).listen(8888);
