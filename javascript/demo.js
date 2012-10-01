60b027b4c3851968d8a6cb0a2df408c268eb27bb
Dan Eastwell <daneastwell@gmail.com>
2012-10-01 16:35:36 +0100

    Added contenteditable to runnable code blocks. Set updated code to run on click of run button #158

--- javascript/demo.js
+++ javascript/demo.js
@@ -10,27 +10,26 @@ jQuery('pre').each(function () {
     $pre.addClass("runnable").wrap('<div class="run" />');
 
     var code = $pre.find('code'),
-        editor = code.text(),
-        readOnly = false,
-        id, output, button;
-
+        output, button;
+    
     // Check code block for runnable keywords and setup output box
     // and run button.
     if (code.text().indexOf('demoElement') > -1 || code.text().indexOf('alert') > -1) {
       
-      id = 'output-' + (examples.index += 1);
-      output = jQuery('<output>click \'run\' button</output>').attr('id', id);
+      examples.index += 1;
+      
+      code.attr('contenteditable', true).attr('id', 'code-' + examples.index);
+      
+      output = jQuery('<output>click \'run\' button</output>').attr('id', 'output-' + examples.index);
       button = jQuery('<button class="eval">Run</button>').data({
           output: output,
-          editor: editor
+          index: examples.index
       });
 
       jQuery(this.parentNode).append(output[0]);
       jQuery(this).append(button[0]);
       
     }else{
-    
-      readOnly = true;
       
       jQuery(this).removeClass("runnable");
       
@@ -41,19 +40,21 @@ jQuery('pre').each(function () {
 jQuery('button.eval')
 .click(function () {
     var button = jQuery(this),
-        code = button.data("editor"),  
+        index =  button.data('index'),  
         output = button.data('output');
 
     output.empty();
     setTimeout(function () {
         var demoElement = 'jQuery("#' + output[0].id + '")[0]',
+            code  = jQuery('#code-' + index).text(),
             $alert  = 'function (msg) {jQuery("#' + output[0].id + '").append("alert: " + msg + "</br/>");}';
 
-        // Add an remove a class when the code is run.
+        // Add and remove a class when the code is run.
         output.addClass('loaded');
         setTimeout(function () {
             output.removeClass('loaded');
         }, 1500);
+        
 
         // Execute the code in a custom scope that includes alert() and $output.
         jQuery.globalEval('(function (demoElement, alert) {' + code + '})(' + demoElement + ', ' + $alert + ')');
