bd54f7a2cfe5fa3345510c6e9bbcc91b578e8a3c
aaron <aaron@crunchbang.(none)>
2013-01-09 11:21:02 +0000

    Add fixtures

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-// Web document samples
+// Web document samples for the test server to serve
 
 exports.documents = {
   html: fs.readFileSync('tests/document.html'),
@@ -12,10 +12,108 @@ exports.documents = {
 // Queries
 
 exports.queries = {
-  simple: {
-    url: 'http://localhost:8889/html',
-    type: 'html',
-    selector: 'title',
-    extract: 'text'
+  html: {
+    simple: {
+      url: 'http://localhost:8889/html',
+      type: 'html',
+      selector: 'title',
+      extract: 'text'
+    },
+    noSelector: {
+      url: 'http://localhost:8889/html',
+      type: 'html'
+    },
+    noExtract: {
+      url: 'http://localhost:8889/html',
+      type: 'html',
+      selector: 'title'
+    },
+    noType: {
+      url: 'http://localhost:8889/html',
+      selector: 'title',
+      extract: 'text'
+    },
+    badSelector: {
+      url: 'http://localhost:8889/html',
+      type: 'html',
+      selector: 'BAD SELECTOR',
+      extract: 'text'
+    },
+    badExtract: {
+      url: 'http://localhost:8889/html',
+      type: 'html',
+      selector: 'title',
+      extract: 'BAD EXTRACT'
+    }
+  },
+  json: {
+    simple: {
+      url: 'http://localhost:8889/json',
+      type: 'json',
+      selector: '.query'
+    },
+    noSelector: {
+      url: 'http://localhost:8889/json',
+      type: 'json'
+    },
+    noType: {
+      url: 'http://localhost:8889/json',
+      selector: '.query'
+    },
+    badSelector: {
+      url: 'http://localhost:8889/json',
+      type: 'json',
+      selector: 'BAD SELECTOR'
+    }
+  },
+  feed: {
+    simple: {
+      url: 'http://localhost:8889/feed',
+      type: 'feed',
+      selector: '.title'
+    },
+    noSelector: {
+      url: 'http://localhost:8889/feed',
+      type: 'feed'
+    },
+    noType: {
+      url: 'http://localhost:8889/feed',
+      selector: '.title'
+    },
+    badSelector: {
+      url: 'http://localhost:8889/feed',
+      type: 'feed',
+      selector: 'BAD SELECTOR'
+    }
+  },
+  xml: {
+    simple: {
+      url: 'http://localhost:8889/xml',
+      type: 'xml',
+      selector: '.CustomerName'
+    },
+    noSelector: {
+      url: 'http://localhost:8889/xml',
+      type: 'xml'
+    },
+    noType: {
+      url: 'http://localhost:8889/feed',
+      selector: '.CustomerName'
+    },
+    badSelector: {
+      url: 'http://localhost:8889/xml',
+      type: 'xml',
+      selector: 'BAD SELECTOR'
+    }
+  },
+  misc: {
+    badUrl: {
+      url: 'BAD URL'
+    },
+    badType: {
+      url: 'http://localhost:8889/html',
+      type: 'BAD TYPE'
+    },
+    emptyQuery: {}
   }
-};
+}
