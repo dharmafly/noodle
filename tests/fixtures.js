56b6e77f6ec769ba235a9229eac2abe8d71c283d
aaron <aaron@crunchbang.(none)>
2013-01-11 14:35:36 +0000

    Add cache:false to all fixtures

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -14,121 +14,157 @@ exports.documents = {
 exports.queries = {
   html: {
     simple: {
-      url: 'http://localhost:8889/html',
-      type: 'html',
-      selector: 'title',
-      extract: 'text'
+      url: "http://localhost:8889/html",
+      type: "html",
+      selector: "title",
+      extract: "text",
+      cache: false
     },
     noSelector: {
       url: 'http://localhost:8889/html',
-      type: 'html'
+      type: 'html',
+      cache: false
     },
     noExtract: {
       url: 'http://localhost:8889/html',
       type: 'html',
-      selector: 'title'
+      selector: 'title',
+      cache: false
     },
     noType: {
       url: 'http://localhost:8889/html',
       selector: 'title',
-      extract: 'text'
+      extract: 'text',
+      cache: false
     },
     badSelector: {
       url: 'http://localhost:8889/html',
       type: 'html',
       selector: 'BAD SELECTOR',
-      extract: 'text'
+      extract: 'text',
+      cache: false
     },
     badExtract: {
       url: 'http://localhost:8889/html',
       type: 'html',
       selector: 'title',
-      extract: 'BAD EXTRACT'
+      extract: 'BAD EXTRACT',
+      cache: false
     }
   },
   json: {
     simple: {
       url: 'http://localhost:8889/json',
       type: 'json',
-      selector: '.query'
+      selector: '.query',
+      cache: false
     },
     noSelector: {
       url: 'http://localhost:8889/json',
-      type: 'json'
+      type: 'json',
+      cache: false
     },
     noType: {
       url: 'http://localhost:8889/json',
-      selector: '.query'
+      selector: '.query',
+      cache: false
     },
     badSelector: {
       url: 'http://localhost:8889/json',
       type: 'json',
-      selector: 'BAD SELECTOR'
+      selector: 'BAD SELECTOR',
+      cache: false
     },
     badParse: {
       url: 'http://localhost:8889/html',
       type: 'json',
-      selector: '.query'
+      selector: '.query',
+      cache: false
     }
   },
   feed: {
     simple: {
       url: 'http://localhost:8889/feed',
       type: 'feed',
-      selector: '.title'
+      selector: '.title',
+      cache: false
     },
     noSelector: {
       url: 'http://localhost:8889/feed',
-      type: 'feed'
+      type: 'feed',
+      cache: false
     },
     noType: {
       url: 'http://localhost:8889/feed',
-      selector: '.title'
+      selector: '.title',
+      cache: false
     },
     badSelector: {
       url: 'http://localhost:8889/feed',
       type: 'feed',
-      selector: 'BAD SELECTOR'
+      selector: 'BAD SELECTOR',
+      cache: false
     },
     badParse: {
       url: 'http://localhost:8889/json',
       type: 'feed',
-      selector: '.title'
+      selector: '.title',
+      cache: false
     }
   },
   xml: {
     simple: {
       url: 'http://localhost:8889/xml',
       type: 'xml',
-      selector: '.CustomerName'
+      selector: '.CustomerName',
+      cache: false
     },
     noSelector: {
       url: 'http://localhost:8889/xml',
-      type: 'xml'
+      type: 'xml',
+      cache: false
     },
     noType: {
       url: 'http://localhost:8889/feed',
-      selector: '.CustomerName'
+      selector: '.CustomerName',
+      cache: false
     },
     badSelector: {
       url: 'http://localhost:8889/xml',
       type: 'xml',
-      selector: 'BAD SELECTOR'
+      selector: 'BAD SELECTOR',
+      cache: false
     },
     badParse: {
       url: 'http://localhost:8889/json',
       type: 'xml',
-      selector: '.CustomerName'
+      selector: '.CustomerName',
+      cache: false
     }
   },
   misc: {
     badUrl: {
-      url: 'BAD URL'
+      url: 'BAD URL',
+      cache: false
     },
     badType: {
       url: 'http://localhost:8889/html',
-      type: 'BAD TYPE'
+      type: 'BAD TYPE',
+      cache: false
     },
     emptyQuery: {}
   }
 };
+
+// Query answers
+
+exports.queries.html.simple.answer = [
+    {
+        "results": [
+            {
+                "text": "css Zen Garden: The Beauty in CSS Design"
+            }
+        ],
+        "created": "2013-01-11T12:14:07.016Z"
+    }
+];
