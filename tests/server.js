22d84895b8af9a618c228c9834e5c1901bfbfaf5
aaron <aaron@crunchbang.(none)>
2013-03-06 14:45:17 +0000

    Test server facilitates testing for link headers

--- tests/server.js
+++ tests/server.js
@@ -2,29 +2,18 @@ var url      = require('url'),
     fixtures = require('./fixtures');
 
 require('http').createServer(function (req, res) {
-  var serve = url.parse(req.url).pathname.split('/')[1],
-      ct    = {
-        'html': 'text/html',
-        'json': 'application/json',
-        'feed': 'application/atom+xml',
-        'xml' : 'text/xml'
-      };
-
+  var serve = url.parse(req.url).pathname.split('/')[1];
 
   if (req.method === 'POST') {
     parsePostData(req, function (data) {
-      res.writeHead(200, {
-        'Content-type': ct['html'],
-        'X-Powered-By': 'Noodle testing server'
-      });
-      res.end('<html><body><h1>was posted</h1></body></html>');
+      var respondWith = (data.foo === 'bar') ? '<h1>was posted</h1>'
+                                             : '<h1>test should fail</h1>';
+      res.writeHead(200, getResponseHeaders('html'));
+      res.end(respondWith);
     });
   } else {
-    res.writeHead(200, {
-      'Content-type': ct[serve],
-      'X-Powered-By': 'Noodle testing server'
-    });
-    res.end(fixtures.documents[serve] || 'specify document type as url path');
+    res.writeHead(200, getResponseHeaders(serve));
+    res.end(fixtures.documents[serve]);
   }
 })
 .listen(8889, function () {
@@ -41,4 +30,18 @@ function parsePostData (req, cb) {
   req.on('end', function () {
     cb(require('querystring').parse(body));
   });
+}
+
+function getResponseHeaders (serve) {
+  var ct = {
+        'html': 'text/html',
+        'json': 'application/json',
+        'feed': 'application/atom+xml',
+        'xml' : 'text/xml'
+      };
+  return {
+    'Content-type': ct[serve],
+    'X-Powered-By': 'Noodle testing server',
+    'Link'        : '<foo>; rel="next",<bar>; rel="last"'
+  };
 }
